32486b3ea0690718b316048d77e4d80e69321e6f
Eric Vallee <eric_vallee@webificservices.com>
2020-01-09 14:39:12 -0500

    Changed score client flow to use latest score code and use shared volume. Added score server to docker network. Fixed some score client orchestration settings

--- launch-services.sh
+++ launch-services.sh
@@ -1,2 +1,7 @@
+OVERTURE_MANIFEST=$(docker volume ls | grep overture_manifests)
+if [ -z "$OVERTURE_MANIFEST" ]; then
+    docker volume create overture_manifests;
+fi
+
 docker-compose up -d
 docker-compose -f docker-compose-auth.yml up -d
