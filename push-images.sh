8261b925e22012f779eafd741594be52bdd1aa5a
Eric Vallee <eric_vallee@webificservices.com>
2020-03-20 16:49:18 -0400

    Progress updating docker hub images scheme

--- push-images.sh
+++ push-images.sh
@@ -1,4 +1,4 @@
-export VERSION=0.2
+export VERSION=0.3
 
 docker-compose build --no-cache;
 
@@ -8,11 +8,5 @@ docker push chusj/overture-score:$VERSION;
 docker tag overture-dev_song:latest chusj/overture-song:$VERSION;
 docker push chusj/overture-song:$VERSION;
 
-docker tag overture-dev_id:latest chusj/overture-id:$VERSION;
-docker push chusj/overture-id:$VERSION;
-
-docker tag overture-dev_id-db:latest chusj/overture-id-db:$VERSION;
-docker push chusj/overture-id-db:$VERSION;
-
-docker tag overture-dev_auth:latest chusj/overture-ego:$VERSION;
-docker push chusj/overture-ego:$VERSION;
+docker tag overture-dev_storage-client:latest chujs/score-client:$VERSION;
+docker push chujs/score-client:$VERSION;
