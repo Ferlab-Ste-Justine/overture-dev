37f0338439a99c5f2221dfa528293cab9bdac073
Eric Vallee <eric_vallee@webificservices.com>
2020-03-26 15:29:57 -0400

    Fixed Score Client bug in logic to push images

--- push-images.sh
+++ push-images.sh
@@ -8,8 +8,11 @@ docker push chusj/overture-score:$VERSION;
 docker tag overture-dev_song:latest chusj/overture-song:$VERSION;
 docker push chusj/overture-song:$VERSION;
 
-docker tag overture-dev_storage-client:latest chusj/score-client:$VERSION;
+docker build --no-cache -t chusj/score-client:$VERSION --target client ./score;
 docker push chusj/score-client:$VERSION;
+
+docker tag overture-dev_auth:latest chusj/redundant-song-auth-dependency:$VERSION;
+docker push chusj/redundant-song-auth-dependency:$VERSION;
 
 (cd song-auth; ./push_image.sh);
 (cd score-auth; ./push_image.sh);
