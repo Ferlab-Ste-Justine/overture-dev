eee1dba68063af15f190e019869efc94ea312ac2
Eric Vallee <eric_vallee@webificservices.com>
2020-04-03 16:06:47 -0400

    Fixes for bug that occured with newly cloned repo

--- launch-services.sh
+++ launch-services.sh
@@ -1,14 +1,17 @@
+#Ensure docker is running in swarm mode
 SWARM=$(docker node ls -q 2>&1 >/dev/null)
 if [[ $SWARM = Error* ]]; then
     echo "Docker must be running in swarm mode to execute this script";
     exit 1;
 fi
 
+#Create the network if it doesn't exist
 OVERTURE_NETWORK_EXISTS=$(docker network ls | grep overture)
 if [ -z "$OVERTURE_NETWORK_EXISTS" ]; then
     docker network create -d overlay --attachable overture;
 fi
 
+#figure out the kind of environment and corresponding docker-compose files
 export ENV=${ENV:-dev}
 
 if [ "$ENV" = "dev" ]; then
@@ -19,6 +22,18 @@ else
     export DOCKER_COMPOSE_AUTH_FILE="dc-auth-prodlike.yml";
 fi
 
+#Create dependant mapped volume paths as docker stack deploy will silently fail to launch some services otherwise
+if [ ! -d "logs" ]; then
+    mkdir -p logs/storage-server-logs;
+    mkdir -p logs/server_logs;
+fi
+
+if [ ! -d "data" ]; then
+    mkdir -p data/minio;
+    mkdir -p data/song;
+fi
+
+#Launch services
 export $(cat .env | xargs)
 if [ "$ENV" = "dev" ]; then
     ./build-local-images.sh;
@@ -34,10 +49,12 @@ export OBJECT_STORAGE_SECRET_KEY=$(cat .env | grep OBJECT_STORAGE_SECRET_KEY | c
 
 #Wait until the object store is responsive
 OBJECT_STORAGE_UNREACHABLE=$(docker run --network overture -e "AWS_ACCESS_KEY_ID=$OBJECT_STORAGE_ACCESS_KEY" -e "AWS_SECRET_ACCESS_KEY=$OBJECT_STORAGE_SECRET_KEY" -e "AWS_DEFAULT_REGION=us-east-1" --rm mesosphere/aws-cli:latest --endpoint-url http://object-storage:8085 s3 ls s3://oicr.icgc.test 2>&1 >/dev/null)
+OBJECT_STORAGE_UNREACHABLE=$(echo $OBJECT_STORAGE_UNREACHABLE | grep "Could not connect to the endpoint URL")
 while [ ! -z "$OBJECT_STORAGE_UNREACHABLE" ]
 do
     sleep 1;
     OBJECT_STORAGE_UNREACHABLE=$(docker run --network overture -e "AWS_ACCESS_KEY_ID=$OBJECT_STORAGE_ACCESS_KEY" -e "AWS_SECRET_ACCESS_KEY=$OBJECT_STORAGE_SECRET_KEY" -e "AWS_DEFAULT_REGION=us-east-1" --rm mesosphere/aws-cli:latest --endpoint-url http://object-storage:8085 s3 ls s3://oicr.icgc.test 2>&1 >/dev/null);
+    OBJECT_STORAGE_UNREACHABLE=$(echo $OBJECT_STORAGE_UNREACHABLE | grep "Could not connect to the endpoint URL")
 done
 
 #Initialize the bucket if needed
