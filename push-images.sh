208d1981ffb772c8bac000523032e3a35dd8c726
Eric Vallee <eric_vallee@webificservices.com>
2020-04-21 11:23:15 -0400

    Bumped version for Overture third-party projects to 0.4. Fixed image pushing script. Updated reverse-proxy repo pointers.

--- push-images.sh
+++ push-images.sh
@@ -1,20 +1,15 @@
-export VERSION=0.3
+export VERSION=0.4
 
 docker build --no-cache -t chusj/overture-song:$VERSION --target server SONG;
-docker build --no-cache -t chusj/overture-score:$VERSION --target server score;
-docker build --no-cache -t chusj/score-client:$VERSION --target client score;
-docker build --no-cache -t chusj/redundant-song-auth-dependency:$VERSION auth-throwaway-dependency;
-
-docker tag overture-dev_storage-server:latest chusj/overture-score:$VERSION;
-docker push chusj/overture-score:$VERSION;
-
-docker tag overture-dev_song:latest chusj/overture-song:$VERSION;
 docker push chusj/overture-song:$VERSION;
 
-docker build --no-cache -t chusj/score-client:$VERSION --target client ./score;
+docker build --no-cache -t chusj/overture-score:$VERSION --target server score;
+docker push chusj/overture-score:$VERSION;
+
+docker build --no-cache -t chusj/score-client:$VERSION --target client score;
 docker push chusj/score-client:$VERSION;
 
-docker tag overture-dev_auth:latest chusj/redundant-song-auth-dependency:$VERSION;
+docker build --no-cache -t chusj/redundant-song-auth-dependency:$VERSION auth-throwaway-dependency;
 docker push chusj/redundant-song-auth-dependency:$VERSION;
 
 (cd song-auth; ./push_image.sh);
